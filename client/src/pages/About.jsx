09748d5290f351bab7add688b3bdc45b7ede1718
abhishek2022003 <abhishek2022003@gmail.com>
2024-02-18 22:32:16 +0530

    final commit

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,18 +4,18 @@ export default function About() {
         <div className='max-w-2xl mx-auto p-3 text-center'>
           <div>
             <h1 className='text-3xl font font-semibold text-center my-7'>
-              About Abhishek's Blog
+              About let's talk
             </h1>
             <div className='text-md text-gray-500 flex flex-col gap-6'>
               <p>
-                Welcome to Abhishek's Blog! This blog was created by Abhishek Kumar Jha
+                Welcome to Abhishek's chatbox! This blog was created by Abhishek Kumar Jha
                 as a personal project to share his thoughts and ideas with the
                 world. Abhishek is a passionate developer who loves to write about
                 technology, coding, and everything in between.
               </p>
   
               <p>
-                On this blog, you'll find weekly articles and tutorials on topics
+                On this chatbox, you'll find weekly articles and tutorials on topics
                 such as web development, software engineering, and programming
                 languages. Abhishek is always learning and exploring new
                 technologies, so be sure to check back often for new content!
